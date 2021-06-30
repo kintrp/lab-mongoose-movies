590d3993dae07b1f92e84ad24457465ffcf26ac1
kintrp <philipp.kintrup@googlemail.com>
2021-06-30 18:57:15 +0200

    Still need 2 do: delete/details

--- seeds.js
+++ seeds.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 const Celebrity = require('./models/Celebrity');
+//const Movie = require('../models/Movie');
 
 require("dotenv/config");
 
 console.log(process.env.MONGODB_URI);
-
 
 mongoose.connect(process.env.MONGODB_URI, {
   useNewUrlParser: true,
